a294b4772641fbf9f79f620d894f3f503d8baca5
Ignacio <ignaciodamiang@gmail.com>
2022-12-16 16:41:06 -0300

    refactor

---
+++ slave-bootstrap.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update
+apt-get install -y wget apt-transport-https
+mkdir -p /etc/apt/keyrings
+wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc
+echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
+apt-get update
+apt-get install -y temurin-11-jdk
+apt-get install -y git
